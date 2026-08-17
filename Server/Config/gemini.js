d423d1bc3588ef6dc4d0755008d2c9ecfc4624db
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2026-08-17 10:19:45 +0530

    Updating Gemini URL to latest flash model

--- Server/Config/gemini.js
+++ Server/Config/gemini.js
@@ -1,5 +1,5 @@
 const Gemini_URL =
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent";
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent";
 
 export const generateGeminiResponse = async ({ prompt, apikey, user }) => {
     try {
